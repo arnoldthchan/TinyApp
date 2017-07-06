d7a5b2cf1fe472a2255406f887402be24fa9ca4b
Arnold Chan <arnoldthchan@gmail.com>
2017-07-06 13:11:10 +0000

    cleaned up and added placeholder registration endpoints

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+//REQUIRED MODULES
 let express = require('express');
 let ejs = require('ejs');
 let bodyParser = require('body-parser');
@@ -16,11 +17,36 @@ let PORT = process.env.PORT || 8080; // default port 8080
 app.set('view engine', 'ejs');
 
 //Database object of shortened URLS and entered URLs
-let urlDatabase = {
+const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
-  'aaaaaa': 'http://www.youtube.com'
+  'aaaaaa': 'http://www.youtube.com',
+  'TestTe': 'http://spotify.com'
 };
+
+//Datebase object of Users containg ID, Email, pass
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  },
+ "user3RandomID": {
+    id: "user3RandomID",
+    email: "user3@example.com",
+    password: "pairsbot-pair"
+  },
+ "user4RandomID": {
+    id: "user4RandomID",
+    email: "user4@example.com",
+    password: "johnbot-feauture"
+  }
+}
 
 // produces a string of 6 random alphanumeric characters:
 function generateRandomString() {
@@ -51,6 +77,60 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+//WHERE THE FORM IS
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new', {username: req.cookies['username']});
+});
+
+//Redirects to actual webpage based on shortenedURL
+app.get('/u/:shortenedURL', (req, res) => {
+  let longURL = urlDatabase[req.params.shortenedURL];
+  // console.log(req.params.shortenedURL); //Logs the generated 6-dig ID
+  if (urlDatabase[req.params.shortenedURL]){
+    res.redirect(longURL);
+  } else {
+    res.statusCode = 404;
+    res.send(`<b><marquee>URL DOES NOT EXIST</marquee></b><p><a href='/urls/new'>Back to Link Shortener </a>`);
+  }
+});
+//Goes to :ID, applies to anything entered after the /urls/
+app.get('/urls/:id', (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  let fullURL = `localhost:8080/urls/${templateVars.shortURL}`;
+  //CHECKS TO SEE IF LENGTH OF URL IS EXACTLY 6 DIGITS LONG
+  if (templateVars.shortURL.length === 6){
+    res.render('urls_show', {
+      templateVars:templateVars,
+      fullURL: fullURL,
+      username: req.cookies['username']
+      });
+  } else { //REDIRECTS BACK TO MAIN PAGE OTHERWISE
+    res.statusCode = 404;
+    res.redirect('/urls');
+  }
+});
+
+//REGISTRATION PAGE
+app.get('/register', (req, res) =>{
+  let templateVars = { shortURL: req.params.id };
+  res.render('register',{
+    templateVars:templateVars,
+    username: req.cookies['username']
+  });
+});
+
+//Cat.
+app.get('/cat', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('content-type', 'text/html');
+  res.render('cat');
+});
+
+//POSTS
+//POSTS
+//POSTS
+
+
 //Uses POST request to submit form data
 app.post('/urls', (req, res) => {
   // console.log(req.body);  // debug statement to see POST parameters
@@ -58,11 +138,6 @@ app.post('/urls', (req, res) => {
   genURL = generateRandomString();
   urlDatabase[genURL] = result.longURL;
   res.redirect(`urls/${genURL}`);
-});
-
-//WHERE THE FORM IS
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new', {username: req.cookies['username']});
 });
 
 //Deletes URL entry based on shortened URL entered
@@ -103,32 +178,24 @@ app.post('/urls/:id', (req, res) =>{
   res.redirect(`/urls/${req.params.id}`);
 });
 
-//Redirects to actual webpage based on shortenedURL
-app.get('/u/:shortenedURL', (req, res) => {
-  let longURL = urlDatabase[req.params.shortenedURL];
-  // console.log(req.params.shortenedURL); //Logs the generated 6-dig ID
-  if (urlDatabase[req.params.shortenedURL]){
-    res.redirect(longURL);
+//POST REGISTER
+app.post('/register', (req, res) =>{
+  let email = req.body.email;
+  let password = req.body.password;
+  if (email && password){
+    genCookie = generateRandomString();//Generates user_id cookie value
+    res.cookie('user_id', genCookie);
+    //ATTEMPTS TO ADD NEW OBJECT
+    // let newNum = Object.keys(users).length+1;
+    users[genCookie] = {
+      id: genCookie,
+      email: email,
+      password: password,
+    };
+    console.log(users);
+    res.redirect(`/urls`);
   } else {
-    res.statusCode = 404;
-    res.send(`<b><marquee>URL DOES NOT EXIST</marquee></b><p><a href='/urls/new'>Back to Link Shortener </a>`);
+    res.statusCode = 400;
+    res.redirect(`/register`);
   }
-});
-
-//:id refers to anything after /url
-app.get('/urls/:id', (req, res) => {
-  let templateVars = { shortURL: req.params.id };
-  let fullURL = `localhost:8080/urls/${templateVars.shortURL}`;
-  res.render('urls_show', {
-    templateVars:templateVars,
-    fullURL: fullURL,
-    username: req.cookies['username']
-    });
-});
-
-//Cat.
-app.get('/cat', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('content-type', 'text/html');
-  res.render('cat');
 });
--- test.js
+++ test.js
@@ -1,9 +1,42 @@
-  function foo() {
-    var x = 1;
-    if (x === 1) {
-      let y = 2;
-      console.log(y);
-    }
-    // console.log('Value of y is ' + y);
+  // function foo() {
+  //   var x = 1;
+  //   if (x === 1) {
+  //     let y = 2;
+  //     console.log(y);
+  //   }
+  //   // console.log('Value of y is ' + y);
+  // }
+  // foo();
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  },
+ "user3RandomID": {
+    id: "user3RandomID",
+    email: "user3@example.com",
+    password: "pairsbot-pair"
+  },
+ "user4RandomID": {
+    id: "user4RandomID",
+    email: "user4@example.com",
+    password: "johnbot-feauture"
   }
-  foo();
+}
+console.log(users);
+users.user5RandomID = {
+  id: 'asd',
+  email: 'asd',
+  password: 'asd'};
+console.log(users)
+// console.log(users.userRandomID);
+// console.log(users.userRandomID.id);
+// console.log(Object.keys(users).length);
+
